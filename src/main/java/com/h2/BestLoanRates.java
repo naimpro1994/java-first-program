e147dc094a6116aeb2f01c22580b5e13ef6369b7
Naim <hannane-naim@hotmail.com>
2022-06-09 17:32:58 +0200

    10 Get the Loan Term Using the Scanner API

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -15,5 +15,14 @@ public class BestLoanRates {
         System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+        float bestRate = getRates(loanTermInYears);
+        if (bestRate == 0.0f)
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
+        else
+            System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
+        scanner.close();
+
     }
 }
