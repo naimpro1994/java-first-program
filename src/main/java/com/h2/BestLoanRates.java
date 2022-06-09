67f74f7a5d1526d52e482025b1a9e4c89849f4ac
Naim <hannane-naim@hotmail.com>
2022-06-09 17:09:36 +0200

    8 Create a New Method getRates()

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class BestLoanRates {
     public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f,2,3.45f,3,2.67f);
+    public static float getRates(int loanTermInYears ){
+        return 0.0f;
+    }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
