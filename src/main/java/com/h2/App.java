d44b95a66ea5776c2008ac2f74bc72dfcc307ef8
Naim <hannane-naim@hotmail.com>
2022-06-09 16:22:53 +0200

    Step 3

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,22 +4,25 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println(doubleTheNumber(5));
     }
 
     public static int doubleTheNumber(int number) {
 
-        return number*2;
+        return number * 2;
 
     }
 
-    private static int add(int numbers[]){
-        return -1 ;
-    }
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers[i]; i++) {
+            sum += numbers[i];
 
+        }
+        return sum;
+
+    }
 }
