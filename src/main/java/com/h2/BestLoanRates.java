3dd0e984a3a75f7d10fc1b181775347d1480731e
Naim <hannane-naim@hotmail.com>
2022-06-09 17:16:59 +0200

    9 Add an Implementation to getRates()

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 public class BestLoanRates {
     public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f,2,3.45f,3,2.67f);
     public static float getRates(int loanTermInYears ){
-        return 0.0f;
+        if (bestRates.containsKey(loanTermInYears))
+            return loanTermInYears;
+        else return 0.0f;
     }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
