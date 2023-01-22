bf23f01a622acb9ba4e5ead913f0507fa5d93db6
Naim <hannane-naim@hotmail.com>
2023-01-22 18:22:57 +0100

    Create a New Method sumOfCredits()

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -5,5 +5,8 @@ public class SavingsCalculator {
     private float[] debits;
 
     public SavingsCalculator (float[] debits, float[] credits) {}
+    private float sumOfCredits(){
+        return 0.0f;
+    }
 
 }
